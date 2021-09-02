aebf1336c6d48ae25aab2efbf6d14fd3dbdf0bb0
= <hungpham28092003@gmail.com>
2021-09-02 17:27:01 +0700

    add B5.py

---
+++ B5.py
@@ -0,0 +1,7 @@
+#nhap vao so thap phan va kiem tra kieu du liệu có hợp lệ ko
+try:
+    print("moi nhap vao so thap phan:")
+    so_10=int(input())
+    print("so thap phan %d chuyen sang bat phan la: %o" %(so_10,so_10))
+except:
+    print("erordata input ")
